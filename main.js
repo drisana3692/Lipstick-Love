77ff6777f768352cfa9acda64f5e51dfb35ef65a
Drisana <drisanabiswas369@gmail.com>
2023-05-05 20:12:43 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,29 @@ function preload() {
 
 }
 function setup() {
-    canvas = createCanvas(300,300);
+    canvas = createCanvas(350,350);
     canvas.center()
+    video= createCapture(VIDEO);
+    video.size(350,350);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw() {
-
+image(video, 0, 0, 350, 350);
 }
 function take_snapshot() {
-    save('hey!');
+    save('hey.png');
+}
+
+function modelLoaded() {
+    console.log('The Model is Loaded!')
+}
+
+function gotPoses(results) {
+if(results.length > 0) {
+    console.log(results);
+    console.log("Nose X Coordinate =" + results[0].pose.net.x);
+    console.log("Nose Y Coordinate" + results[0].pose.net.y);
+}
 }
