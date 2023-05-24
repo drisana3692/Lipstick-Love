8e01334cf8da17b2e7ceb7f8a1b45ad6f32b8c45
Drisana <drisanabiswas369@gmail.com>
2023-05-24 16:50:10 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX = 0;
+noseY = 0;
 function preload() {
-
+lipstick = loadImage('https://i.postimg.cc/j5rjqHbH/LIPPS.png');
 }
 function setup() {
     canvas = createCanvas(350,350);
@@ -12,6 +14,9 @@ function setup() {
 }
 function draw() {
 image(video, 0, 0, 350, 350);
+stroke(173, 216, 230);
+fill(255, 127, 80);
+image(lipstick, noseX, noseY, 40, 20);
 }
 function take_snapshot() {
     save('hey.png');
@@ -26,5 +31,7 @@ if(results.length > 0) {
     console.log(results);
     console.log("Nose X Coordinate =" + results[0].pose.nose.x);
     console.log("Nose Y Coordinate" + results[0].pose.nose.y);
+    noseX = results[0].pose.nose.x-25;
+    noseY = results[0].pose.nose.y+13;
 }
 }
